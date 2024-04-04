fe3ec70b742d2b96fa9cb89a6d4e185e2fe35608
Damyy17 <90212650+Damyy17@users.noreply.github.com>
2024-04-05 01:13:56 +0300

    Initial Commit

---
+++ dialdiver/dialdiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  dialdiverApp.swift
+//  dialdiver
+//
+//  Created by Damian Grosu on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct dialdiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dialdiverUITests/dialdiverUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  dialdiverUITestsLaunchTests.swift
+//  dialdiverUITests
+//
+//  Created by Damian Grosu on 05.04.2024.
+//
+
+import XCTest
+
+final class dialdiverUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
